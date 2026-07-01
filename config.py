16e4a9cac1ee3383893dd0b7d5f19bf255c1ea19
ramubry-alt <ramubry@gmail.com>
2026-07-01 11:43:49 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+</> Python
+
+EMAIL_FROM = "YOUR_GMAIL@gmail.com"
+EMAIL_TO = "ramubry@gmail.com"
+
+EMAIL_APP_PASSWORD = "PASTE_YOUR_APP_PASSWORD_HERE"
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
