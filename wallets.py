eaf659c9343061f30e41fec6abb311db9af1a25b
ramubry-alt <ramubry@gmail.com>
2026-07-02 09:28:14 -0600

    Update wallets.py

--- wallets.py
+++ wallets.py
@@ -6,10 +6,11 @@ WALLETS_5 = [
     "0xe9a6ed2e4d4ee8ce47cd47cac834746dc4cf627b",
 ]
 
-TOP_25 = WALLETS_5 = [
-     "0x0346afae2603313d2bbee96b628536c8cbe352a5",
+TOP_25 = [
+    "0x0346afae2603313d2bbee96b628536c8cbe352a5",
     "0x9f2fe025f84839ca81dd8e0338892605702d2ca8",
     "0x7c1ee865a785de4c00ee90ed86a38489fb8bbab3",
     "0xfea31bc088000ff909be1dfd8d0e3f2c7ef2d227",
     "0xe9a6ed2e4d4ee8ce47cd47cac834746dc4cf627b",
-] 
+    # add more wallets here until you reach 25 total
+]
