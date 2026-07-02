ff4e68279c132139d6c191755e5077da424769c6
ramubry-alt <ramubry@gmail.com>
2026-07-02 06:43:42 -0600

    Update wallets.py

--- wallets.py
+++ wallets.py
@@ -2,10 +2,8 @@
 
 WALLETS = {
     "surfandturf": "0x9f2fe025f84839ca81dd8e0338892605702d2ca8",
-
-    # Fill these in as we identify them
-    "goallineghost": "",
-    "candlehammerdrums": "",
-    "ndb1": "",
-    "oneger": "",
+    "goallineghost": "0x0346afae2603313d2bbee96b628536c8cbe352a5",
+    "candlehammerdrums": "0x7c1ee865a785de4c00ee90ed86a38489fb8bbab3",
+    "ndb1": "0xfea31bc088000ff909be1dfd8d0e3f2c7ef2d227",
+    "oneger": "0xe9a6ed2e4d4ee8ce47cd47cac834746dc4cf627b",
 }
