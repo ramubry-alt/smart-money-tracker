a28f684c3dfb218d28c05d7189b2709ac0ad02a8
ramubry-alt <ramubry@gmail.com>
2026-07-02 06:50:26 -0600

    Update wallets.py

--- wallets.py
+++ wallets.py
@@ -1,9 +1,9 @@
-# Tracked wallets
+WALLETS_5 = [
+    "0x0346afae2603313d2bbee96b628536c8cbe352a5",
+    "0x9f2fe025f84839ca81dd8e0338892605702d2ca8",
+    "0x7c1ee865a785de4c00ee90ed86a38489fb8bbab3",
+    "0xfea31bc088000ff909be1dfd8d0e3f2c7ef2d227",
+    "0xe9a6ed2e4d4ee8ce47cd47cac834746dc4cf627b",
+]
 
-WALLETS = {
-    "surfandturf": "0x9f2fe025f84839ca81dd8e0338892605702d2ca8",
-    "goallineghost": "0x0346afae2603313d2bbee96b628536c8cbe352a5",
-    "candlehammerdrums": "0x7c1ee865a785de4c00ee90ed86a38489fb8bbab3",
-    "ndb1": "0xfea31bc088000ff909be1dfd8d0e3f2c7ef2d227",
-    "oneger": "0xe9a6ed2e4d4ee8ce47cd47cac834746dc4cf627b",
-}
+TOP_25 = WALLETS_5  # placeholder for now (we’ll expand later)
