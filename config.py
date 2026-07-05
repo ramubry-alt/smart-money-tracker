83e89579e35b33e5fe6939a2fdb5ed12e86c9222
ramubry-alt <ramubry@gmail.com>
2026-07-05 15:34:26 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
-EMAIL_FROM = "ramubry@gmail.com"
-EMAIL_TO = "ramubry@gmail.com"
+import os
 
-EMAIL_APP_PASSWORD = "jzck zcwj qcnb ydos"
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
+
+
+EMAIL_FROM = os.getenv("EMAIL_FROM", "ramubry@gmail.com")
+EMAIL_TO = os.getenv("EMAIL_TO", "ramubry@gmail.com")
+
+EMAIL_APP_PASSWORD = os.getenv("EMAIL_APP_PASSWORD", "")
 
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
